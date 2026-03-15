c32feecdad93ac0c1d36239d1d8cc2396ee4b672
keunwoo kim <keun1999@outlook.kr>
2026-03-15 23:15:34 +0900

    효과음 추가 코드구현 삽입 및 주석처리

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using System.Media; // 효과음 추가를 위한 코드 삽입
 
 namespace CatchButton
 {
@@ -15,6 +16,11 @@ namespace CatchButton
         int score = 0; // 점수 표시를 위한 컨텍스트 선언
         int missCount = 0; //버튼을 놓친 횟수 저장 변수
         Size initialSize; // 처음 버튼 크기를 기억하는 변수
+
+        //SoundPlayer escapeSound = new SoundPlayer("escape.wav"); // 도망 효과음
+        //SoundPlayer catchSound = new SoundPlayer("catch.wav");   // 잡기 효과음
+
+
         public Form1()
         {
             InitializeComponent();
@@ -45,6 +51,8 @@ namespace CatchButton
             score -= 10;
             missCount++; // 버튼 놓칠때 횟수 적용 코드
 
+            //escapeSound.Play(); //도망 효과음 추가 
+
             this.Text = $"현재 점수: {score}점";
 
             if (missCount >= 20) //게임 오버 체크 코드
@@ -61,6 +69,10 @@ namespace CatchButton
         {
             //점수 추가 표시
             score += 100;
+
+            //catchSound.Play(); //잡기 효과음
+
+
             this.Text = $"현재 점수: {score}점";
 
             // 2. 난이도 조절: 버튼 크기를 10% 축소 (현재 크기의 90%로 설정)
@@ -80,7 +92,7 @@ namespace CatchButton
             RunButton.Size = initialSize; // 버튼 크기를 처음 상태로 복구
             RunButton.Location = new Point(100, 100); // 버튼 위치를 적당한 곳으로 이동
 
-            RunButton.Enabled = true;//게임 재시작 버튼 활성화
+            RunButton.Enabled = true;//게임 재시작 버튼 
             this.Text = "게임을 다시 시작합니다!";
         }
 
